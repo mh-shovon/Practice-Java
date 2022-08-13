d93156208483211df05b23a3dab2230bae3ce7ed
MEHEDI HASAN SHOVON <shovon33266@gmail.com>
2022-08-13 16:50:19 +0600

    Variable Declare

---
+++ src/VariableandInputs.java
@@ -0,0 +1,9 @@
+public class VariableandInputs {
+    public static void main(String[] args) {
+        int a=7;
+        int b=8;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
+
